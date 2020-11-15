ca9ec1a5923983cbecb59b3d456994bca4d3a0d9
Vladimir Ubogovich <vladimir.ubogovich@metronom.com>
2020-11-15 23:11:32 +0100

    Solve boring numbers

--- src/kickstart2020/h/BoringNumbers.kt
+++ src/kickstart2020/h/BoringNumbers.kt
@@ -1,47 +1,32 @@
 package kickstart2020.h
 
+import kotlin.math.max
+
+private val power5 = Array(20) { 1L }
+
 fun main() {
     val inputFileName = "src/kickstart2020/h/BoringNumbers.in"
     java.io.File(inputFileName).takeIf { it.exists() }?.also { System.setIn(it.inputStream()) }
+
+    for (i in 1 until power5.size) power5[i] = power5[i - 1] * 5
 
     for (case in 1..readLine()!!.toInt()) {
         val (l, r) = readLine()!!.split(' ').map { it.toLong() }
         val res = boring(r) - boring(l - 1)
         println("Case #$case: $res")
     }
-
-    // TODO solve case with leading zeros
-    var r2 = 0L
-    for (i in 1..1000000) {
-        val r1 = boring(i.toLong())
-
-        if (i.toString().mapIndexed { index, ch -> index % 2 != ch.toInt() % 2 }.all { it }) {
-            r2++
-        }
-
-        if (r1 != r2) {
-            println("$i $r1 <> $r2")
-            break
-        }
-    }
 }
 
-private fun boring(k: Long, a: Int = 5): Long {
-    var l = 0
-    var m = 1L
+private fun boring(k: Long): Long {
+    val s = k.toString()
+    return calculate(s) + (1 until s.length).map { power5[it] }.sum()
+}
 
-    while (k / m >= 100) {
-        l++
-        m *= 10
-    }
-
-    val na = if (l > 0) 0 else 5
-
-    return when {
-        k < 10 && a > 0 -> (k + 1) / 2
-        k < 100 && k / 10 % 2 == 0L -> (k / 20) * 5 + a
-        k < 100 -> (k / 20) * 5 + (k % 10) / 2 + a + 1
-        k / m / 10 % 2 == 1L && k / m % 2 == 0L -> boring(k / m - 1, 0) * boring(m - 1, na) + boring(m * 10 - 1) + boring(k % m, na)
-        else -> boring(k / m - 1, 0) * boring(m - 1, na) + boring(m * 10 - 1)
+private fun calculate(s: String): Long {
+    return when (s.length) {
+        1 -> (s.toLong() + 1) / 2
+        2 -> s.toLong() / 20 * 5 + if (s[0].toInt() % 2 > 0) (s.substring(1).toLong()) / 2 + 1 else 0
+        else -> max(0, s.take(2).toInt() - 1).toString().let { if (it.length == 2) calculate(it) * power5[s.length - 2] else 0 } +
+            if (s[0].toInt() % 2 > 0 && s[1].toInt() % 2 == 0) calculate(s.substring(2)) else 0
     }
 }
